5f467d89bd3b437550147de1116a3a053b2f167c
Domingo Pineda <domingoantonio2072@gmail.com>
2022-03-06 18:16:49 -0400

    Cracion del LoginController

---
+++ HorasExtrasAPI/HorasExtrasAPI/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using HorasExtrasAPI.Context;
+using HorasExtrasAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorasExtrasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HorasExtrasDbContext _context;
+       
+
+        public LoginController( IConfiguration configuration, HorasExtrasDbContext context) 
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Login([FromBody] UserLogin userLogin)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+    }
+}
---
+++ HorasExtrasAPI/HorasExtrasAPI/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorasExtrasAPI.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        public string user { get; set; }
+        [Required]
+        public string Pwd { get; set; }
+    }
+}
